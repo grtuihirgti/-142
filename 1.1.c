41c0dd7065bd93f26e4defe9b03b0ffc86534ef5
Назаров Даниил цкб_142 <nadana2312@gmail.com>
2026-09-22 18:57:20 +0300

    1.1.c

---
+++ 1.1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <math.h>
+/**
+
+@brief Вычисляет значение функции A по заданной формуле
+@PARAM x значение параметра x
+@PARAM y значение параметра y
+@return рассчитанное значение
+*/
+double A(const double x, const double y);
+/**
+
+@brief Вычисляет значение функции B по заданной формуле
+@PARAM x значение параметра x
+@PARAM y значение параметра y
+@return рассчитанное значение
+*/
+double B(const double x, const double y);
+/**
+
+@brief точка входа в программму
+@return возвращает 0, если программа выполнена корректно
+*/
+int main()
+{
+const double x = 0.335;
+const double y = 0.025;
+printf("a = %.3f\n", A(x, y));
+printf("b = %.3f", B(x, y));
+return 0;
+}
+double A(const double x, const double y)
+{
+return 1 + x + (pow(x,2))/2 + (pow(x,3))/3 + (pow(x,4))/4;
+}
+
+double B(const double x, const double y)
+{
+return x*(sin(pow(x, 3)) + cos(pow(y,2)));
+}
